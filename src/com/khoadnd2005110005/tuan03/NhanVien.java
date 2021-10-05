a9c27e24f587491af4b0769e92eca1ec8e98cbab
lightbullet001 <lightbullet001@gmail.com>
2021-10-05 21:03:33 +0700

    cap nhat tuan 3

---
+++ src/com/khoadnd2005110005/tuan03/NhanVien.java
@@ -0,0 +1,23 @@
+package com.khoadnd2005110005.tuan03;
+
+public class NhanVien {
+    String TenNhanVien;
+    double Luong;
+    String diachi;
+    String bophanlamviec;
+    String NgaySinh;
+    NhanVien(){
+    }
+    NhanVien(String ten, double l, String d, String bophan, String sn){
+        TenNhanVien = ten;
+        Luong = l;
+        diachi = d;
+        bophanlamviec = bophan;
+        NgaySinh = sn;
+    }
+    void inThongtin(){
+        
+    }
+
+    
+}
---
+++ src/com/khoadnd2005110005/tuan03/NhanVienTestDrive.java
@@ -0,0 +1,14 @@
+package com.khoadnd2005110005.tuan03;
+
+public class NhanVienTestDrive {
+    
+    public static void main(String[] args) {
+        
+        NhanVien nhanvien;
+        
+        nhanvien = new NhanVien("Ngo Minh Thai", 6000000, "10 Hoa Sua", "DH Gia Dinh", "30-08-2002");
+        
+        System.out.println("Ten Nhan Vien: " + nhanvien.TenNhanVien + "Luong: " + nhanvien.Luong + "Dia Chi: " +nhanvien.diachi + "Bo phan lam viec: " + nhanvien.bophanlamviec + "Ngay Sinh: " + nhanvien.NgaySinh);
+    }
+    
+}
